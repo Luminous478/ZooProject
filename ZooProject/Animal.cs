3eea10c02e110b90b833227b62df9684fa61d742
Oscar Lundstedt <lundstedto@gmail.com>
2026-05-25 13:09:09 +0200

    added more comments

--- ZooProject/Animal.cs
+++ ZooProject/Animal.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 
+// Class for all animals in the program
 class Animal
 {
+    
     public int Id { get; set; }
+
     public string Name { get; set; }
+
+    
     public string Species { get; set; }
+
+    
     public DateTime BirthDate { get; set; }
+
+    
     public DietType Diet { get; set; }
+
+    
     public EnvironmentType Environment { get; set; }
+
+    
     public int LifeExpectancy { get; set; }
+
+  
     public DateTime LastFed { get; set; }
 
+    // Constructor used when creating a new animal
     public Animal(int id, string name, string species,
         DateTime birthDate,
         DietType diet,
         EnvironmentType environment,
         int lifeExpectancy)
     {
+        // Save values into the object
         Id = id;
         Name = name;
         Species = species;
@@ -25,29 +42,37 @@ class Animal
         Environment = environment;
         LifeExpectancy = lifeExpectancy;
 
+        // Sets the animal as fed 10 hours ago
         LastFed = DateTime.Now.AddHours(-10);
     }
 
+    // Returns information about the animal as text
     public virtual string GetInfo()
     {
         return $"ID: {Id}, Name: {Name}, Species: {Species}, Age: {GetAge(DateTime.Now)}";
     }
 
+    // Updates the feeding time
     public void Feed()
     {
         LastFed = DateTime.Now;
     }
 
+    // Checks if the animal needs food
     public bool NeedsFeeding(DateTime currentTime)
     {
+        // Returns true if more than 8 hours have passed
         return (currentTime - LastFed).TotalHours > 8;
     }
 
+    // Checks if the animal is still alive
     public bool IsAlive(DateTime currentDate)
     {
+        // Returns true if the animal's age is less than life expectancy
         return GetAge(currentDate) < LifeExpectancy;
     }
 
+    // Calculates the animal's age
     public int GetAge(DateTime currentDate)
     {
         return currentDate.Year - BirthDate.Year;
--- ZooProject/Zoo.cs
+++ ZooProject/Zoo.cs
@@ -1,25 +1,31 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic; 
+using System.Linq; 
 
+// Class that manages all animals in the zoo
 class Zoo
 {
+    // List that stores all animals
     private List<Animal> animals = new List<Animal>();
 
+    // Adds a new animal to the list
     public void AddAnimal(Animal animal)
     {
         animals.Add(animal);
     }
 
+    // Removes an animal from the list
     public void RemoveAnimal(Animal animal)
     {
         animals.Remove(animal);
     }
 
+    // Returns all animals in the zoo
     public List<Animal> GetAllAnimals()
     {
         return animals;
     }
 
+    // Searches animals by species
     public List<Animal> SearchBySpecies(string species)
     {
         return animals
@@ -27,6 +33,7 @@ class Zoo
             .ToList();
     }
 
+    // Searches animals by diet type
     public List<Animal> SearchByDiet(DietType diet)
     {
         return animals
@@ -34,6 +41,7 @@ class Zoo
             .ToList();
     }
 
+    // Searches animals by environment
     public List<Animal> SearchByEnvironment(EnvironmentType env)
     {
         return animals
@@ -41,6 +49,7 @@ class Zoo
             .ToList();
     }
 
+    // Finds an animal by its id
     public Animal GetAnimalById(int id)
     {
         return animals.FirstOrDefault(a => a.Id == id);
